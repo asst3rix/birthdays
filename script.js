df4a79f23263670706241af0f88c6bd47ec27bad
asst3rix_WC <185900998+asst3rix@users.noreply.github.com>
2026-09-23 13:58:16 +0200

    Set comment en alert in English.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Import des SDK Firebase Web (Modular v10+)
+// Importation of SDK Firebase Web (Modular v10+).
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js";
 
-// 1. Vos identifiants Firebase (collez votre config ici)
 const firebaseConfig = {
     apiKey: "AIzaSyB_sQfn8l6XcW1zlcIz5Rhudyy9B-13Jh8",
     authDomain: "birthdays-notifications.firebaseapp.com",
@@ -12,24 +11,23 @@ const firebaseConfig = {
     appId: "1:1040189479045:web:0b3dfce7b4fdf94709f1ec"
 };
 
-// Initialize Firebase
+// Initializing Firebase.
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-// 2. Gestion du clic sur le bouton
+// Activating notifications.
 const btn = document.querySelector('#btn-notifications');
 const text = document.querySelector('#token');
 btn.addEventListener('click', async () => {
     try {
-        // Demande la permission à l'utilisateur
+        // Ask permission to the user.
         const permission = await Notification.requestPermission();
 
         if (permission === 'granted') {
-            // Enregistrement explicite du Service Worker (Très important pour iOS)
+            // We store the Service Worker (important for iOS).
             const register = await navigator.serviceWorker.register('./firebase-messaging-sw.js');
 
-            // Récupère le token FCM (Clé VAPID nécessaire pour le Web)
-            // On génèrera cette clé à l'étape suivante dans la console Firebase
+            // We store the FCM token (vapid key for the web).
             const token = await getToken(messaging, {
                 vapidKey: 'BLuuxjr5-a7EbeV8-px_ob0yYuQzz4G74oRvoSisMeB0eikOgo2P20heWGe5uu8ul2oujh49r3S4vgkISWO7WtA',
                 serviceWorkerRegistration: register
@@ -38,19 +36,19 @@ btn.addEventListener('click', async () => {
             if (token) {
                 text.textContent = token;
             } else {
-                alert("Impossible d'obtenir le token.");
+                alert("Can't retrieve the token.");
             }
             /*console.log('Voici votre Token FCM :', token);
             alert('Notifications activées ! Token généré (voir console)');*/
         } else {
-            alert('Permission refusée.');
+            alert('Permission refused.');
         }
     } catch (error) {
-        alert('Erreur lors de la récupération du token : ' + error.message);
+        alert('Error while trying to get the FCM token: ' + error.message);
     }
 });
 
-// Écoute si une notification arrive pendant que l'app est ouverte
+// Listen if a notification arrives while the app is open.
 onMessage(messaging, (payload) => {
-    alert(`[Test Réussi] ${payload.notification.title} : ${payload.notification.body}`);
+    alert(`[Test succesfull] ${payload.notification.title} : ${payload.notification.body}`);
 });
